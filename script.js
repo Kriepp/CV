bc3c3a0745ccb73fb7fdb041d4e39887eff366dd
Kriepp <113834453+Kriepp@users.noreply.github.com>
2024-02-17 18:23:38 +0200

    Created js file
    I created the js file to do the carousel for the projects side of the page.

---
+++ script.js
@@ -0,0 +1,17 @@
+const projects = {
+  title: 'Projects',
+  projects: [
+    {
+      title: 'Project 1',
+      description: 'This is the first project',
+      link: 'https://www.google.com',
+    },
+    {
+      title: 'Project 2',
+      description: 'This is the second project',
+      link: 'https://www.google.com',
+    },
+  ],
+};
+
+document.getElementById('projectsTitle').innerHTML = projects.title;
