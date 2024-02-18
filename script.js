64b245dce4673a5ca909ccafd8d414dbaf0fefa3
Kriepp <113834453+Kriepp@users.noreply.github.com>
2024-02-18 16:08:57 +0200

    Carousel
    Trying to make the carousel work

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 let liItems = document.querySelectorAll('ul li');
 let index = 0;
+const prevButton = document.getElementById('prev-btn');
+const nextButton = document.getElementById('next-btn');
+
 window.show = function (increase) {
   index = +increase;
   index = Math.min(Math.max(index, 0), liItems.length - 1);
   liItems[index].scrollIntoView({ behavior: 'smooth' });
 };
+
+// Not working
+prevButton.addEventListener('click', function () {
+  show(-1);
+  console.log(index);
+});
+
+// Working but not changing behavior
+nextButton.addEventListener('click', function () {
+  show(1);
+  console.log(index);
+});
