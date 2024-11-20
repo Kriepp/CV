fb90eca2ee63c4122009e2bf9ac0dc6f353bc808
Kriepp <satmarclaudiu83@gmail.com>
2024-11-20 15:28:59 +0200

    Trying to get db

--- script.js
+++ script.js
@@ -1,36 +1,31 @@
-fetch('http://localhost:5000/test')
-  .then((response) => response.json())
-  .then((data) => console.log(data))
-  .catch((error) => console.error('Fetch error:', error));
-
 fetch('http://localhost:5000/education')
   .then((response) => response.json())
   .then((education) => {
-    const containerEdu = document.getElementById('education-container');
+    const container = document.getElementById('education-container');
     education.forEach((course) => {
       const educationDiv = document.createElement('div');
       educationDiv.innerHTML = `
         <h2>${course.title}</h2>
         <p>${course.description}</p>
-        <a href="${course.URL}">View course</a> 
+        <a href="${course.URL}" target="_blank">Learn More</a>
       `;
-      containerEdu.appendChild(educationDiv);
+      container.appendChild(educationDiv);
     });
+    console.log(education);
   })
   .catch((error) => console.error('Error fetching education:', error));
 
 fetch('http://localhost:5000/projects')
   .then((response) => response.json())
   .then((projects) => {
-    const container = document.getElementById('project-container');
+    const container = document.getElementById('projects-container');
     projects.forEach((project) => {
       const projectDiv = document.createElement('div');
       projectDiv.innerHTML = `
         <h2>${project.title}</h2>
         <p>${project.description}</p>
         <p><strong>Technologies:</strong> ${project.technology.join(', ')}</p>
-        <a href="${project.URL}">View Project</a> 
-        <img src="${project.image}" alt="${project.title}"> 
+        <a href="${project.URL}" target="_blank">View Project</a>
       `;
       container.appendChild(projectDiv);
     });
